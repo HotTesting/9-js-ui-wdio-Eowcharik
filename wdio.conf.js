5747e8f50e08e12e706561bef0b0695add3e64f8
eowcharik <owch54@gmail.com>
2019-12-10 16:04:00 +0200

    config fix

--- wdio.conf.js
+++ wdio.conf.js
@@ -23,8 +23,8 @@ exports.config = {
   // directory is where your package.json resides, so `wdio` will be called from there.
   //
   specs: [
-    // "./test/specs/**/*.ts"
-    "./test/specs/order.ts"
+    "./test/specs/**/*.ts"
+    // "./test/specs/order.ts"
     // "test/specs/register.ts"
     // "test/specs/smoke.ts"
   ],
